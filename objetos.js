ed3c3c52d1a8a311a7a0c909f8921999dbcabcf1
ivan caro <ivanfelipecaro98@gmail.com>
2022-04-10 18:23:56 -0500

    e.g.: Objets init

--- objetos.js
+++ objetos.js
@@ -6,10 +6,10 @@ function auto (MARCA, MODELO, ANNIO){
     this.annio = ANNIO;
   }
   var autos = [];
-  for(let i = 0 ; i < 3 ; i++){
-    var marca = prompt("Ingresa la marca del auto");
-    var modelo = prompt("Ingresa el modelo del auto");
-    var annio = prompt("Ingresa el año del auto");
+  for(let i = 0 ; i < 2 ; i++){
+    var marca = prompt("Ingresa la marca del auto: ");
+    var modelo = prompt("Ingresa el modelo del auto: ");
+    var annio = prompt("Ingresa el año del auto: ");
     autos.push(new auto (marca, modelo, annio));
   }
   
