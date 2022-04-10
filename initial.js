959aac37c859b88ad666f8b2e2463bc6e9e8f8a0
ivan caro <ivanfelipecaro98@gmail.com>
2022-04-10 16:09:53 -0500

    Commit inicial JS

---
+++ initial.js
@@ -0,0 +1,8 @@
+console.log("Hello world!");
+
+const prompt = require('prompt-sync')();
+
+var fname = prompt('Enter your first name: ');
+var lname = prompt('Enter your last name: ');
+
+console.log("Your full name is",fname,lname);
---
+++ variables.js
@@ -0,0 +1,8 @@
+let vehicles = ["Fiesta", "Taurus", "Explorer"];
+
+let myCar = vehicles[2];
+
+console.log(myCar);
+
+vehicles.push('Cloe');
+console.log(vehicles);
