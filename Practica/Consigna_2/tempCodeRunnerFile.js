e82724ad25428b5d7ae649080857898bdadaf361
ivan caro <ivanfelipecaro98@gmail.com>
2022-04-22 19:12:07 -0500

    Corrección consigna 2 JS Backend

--- Practica/Consigna_2/tempCodeRunnerFile.js
+++ Practica/Consigna_2/tempCodeRunnerFile.js
@@ -1,11 +1,4 @@
-const fs = require('fs')
-   let contenidoArchivo;
-        async function saveArchivo(){
-            try{
-                contenidoArchivo = await fs.promises.readFile('./Practica/Consigna_2/productos_resultado.txt','utf-8')
-                console.log('Archivo leido correctamente')
-            }catch(err){
-                console.log('Error de lectura')
-            }
-        }
-        saveArchivo()
+console.group('deleteAll');
+        await carrito.deleteAll()
+        console.log(await carrito.getAll())
+    console.groupEnd();
